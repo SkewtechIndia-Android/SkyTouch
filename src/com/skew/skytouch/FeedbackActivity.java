7e4e0f1118b1f26c8b82e7da4ec9b6c74ad9e001
vinodskadam <vinod.s.kadam@gmail.com>
2015-05-12 17:16:13 +0530

    code added to check internet connection

--- src/com/skew/skytouch/FeedbackActivity.java
+++ src/com/skew/skytouch/FeedbackActivity.java
@@ -1,30 +1,67 @@
 package com.skew.skytouch;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.provider.Settings.Secure;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class FeedbackActivity extends ActionBarActivity {
 	WebView webView;
 	String deviceid;
+	InputStream is=null;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+    	nameValuePairs.add(new BasicNameValuePair("userid",deviceid));
+		// creating connection detector class instance
 		setContentView(R.layout.activity_feedback);
 		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 		StrictMode.setThreadPolicy(policy);
 		deviceid = Secure.getString(getApplicationContext().getContentResolver(), Secure.ANDROID_ID);
-		String url = "http://meghae.com/resto/admin/feedback.php"+"?"+"deviceid="+deviceid;
-		webView = (WebView) findViewById(R.id.webViewfeedback);
-		webView.getSettings().setJavaScriptEnabled(true);
-		webView.setWebViewClient(new WebViewClient());
-		webView.loadUrl(url);
+		
+		Log.e("pass 1", "connection success ");
+		 ConnectivityManager connec = (ConnectivityManager)getSystemService(getBaseContext().CONNECTIVITY_SERVICE);
+	           // Check for network connections
+	            if (connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.CONNECTED ||
+		                 connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.CONNECTED ) {
+	                // if connected with internet
+	            	String url = "http://meghawatersuppliers.com/Restro/admin/feedback.php"+"?"+"deviceid="+deviceid+"&restoid=rs";
+	    			webView = (WebView) findViewById(R.id.webViewfeedback);
+	    			webView.getSettings().setJavaScriptEnabled(true);
+	    			webView.setWebViewClient(new WebViewClient());
+	    			webView.loadUrl(url);
+	    	    }else if ( 
+	              connec.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.DISCONNECTED ||
+	              connec.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.DISCONNECTED  ) {
+	              Toast.makeText(this, " No Internet Connection", Toast.LENGTH_LONG).show();
+	              Intent intent = new Intent(getApplicationContext(),FoodMenuActivity.class);
+				  startActivity(intent);
+				  finish();
+	            }
 	}
 	public void onBackPressed() {
 	    // Do Here what ever you want do on back press;
